fa8622544f6a2af259c20d7c238f4050bd127559
Kamesuta <kamesuta@gmail.com>
2017-09-24 18:40:05 +0900

    #

--- js/wide.js
+++ js/wide.js
@@ -1,10 +1,10 @@
+---
+---
+
 $(document).ready(function() {
     (function() {
+        var args = arguments;
         var header = $(".header");
-        var layer_1 = $('#wide_layer_1');
-        var layer_2 = $('#wide_layer_2');
-        var layer_3 = $('#wide_layer_3');
-        var layer_4 = $('#wide_layer_4');
         var scl = -0.001;
 
         var fps = 10; // 30fpsに制限
@@ -38,34 +38,26 @@ $(document).ready(function() {
             }
             lastX = dx, lastY = dy;
             
-            layer_1.css('transform', 'translate3d('+dx*scl*-1+'px, '+dy*scl*-3+'px, 0)');
-            layer_2.css('transform', 'translate3d('+dx*scl*1+'px, '+dy*scl*1+'px, 0)');
-            layer_3.css('transform', 'translate3d('+dx*scl*3+'px, '+dy*scl*3+'px, 0)');    
-            layer_4.css('transform', 'translate3d('+dx*scl*5+'px, '+dy*scl*5+'px, 0)');    
+            Array.prototype.forEach.call(args, function(node) {
+                node.target.css('transform', 'translate3d('+dx*scl*node.scale+'px, '+dy*scl*node.scale+'px, 0)');
+            });
             
             // 次のanimationを登録
             setTimeout(animating, frameTime);
         }
-    })(
-
-    );
+    }(
+        { target: $('#wide_layer_1'), scale: -1 },
+        { target: $('#wide_layer_2'), scale: 1 },
+        { target: $('#wide_layer_3'), scale: 5 },
+        { target: $('#wide_layer_4'), scale: 5 }
+    ));
 
     (function() {
-        var l = $("body"),
-        r = $("main"),
-        d = ($("#l-header"), $("#l-footer"), $("#l-wrap"), $("#l-intro"), $("#l-loading"), $("#js-slider__target"), $(".wide_wrap .wide_layer")),
-        m = !1,
-        h = !1,
-        p = null,
-        i = 0,
-        _ = $("#main-intro"),
-        v = ($("#sp-flg").css("display"), "is-loaded"),
-        y = "is-active";
-        M = ($(".js-calcWidth__target"), $(".js-bubble__target"));
-
-        b = new TimelineMax({
+        var b = new TimelineMax({
             repeat: 300,
-            onUpdate: f,
+            onUpdate: function() {
+                $('#wide_ctrl_slider').slider("value", b.progress());
+            },
             delay: 1,
             yoyo: true
         });
@@ -80,7 +72,7 @@ $(document).ready(function() {
                 ease: Linear.easeNone
             });
             return e
-        }($(".wide_wrap .wide_layer")),
+        }($(".wide-layer")),
         function(d) {
             TweenLite.set(d, {
                 x: 0,
@@ -92,50 +84,7 @@ $(document).ready(function() {
                 ease: Linear.easeNone
             });
             return e
-        }($(".wide_wrap"))]);
-        
-        function f() {
-            $('#wide_ctrl_slider').slider("value", b.progress());
-            var e = $("#wide_ctrl_slider").slider("value");
-            "block" == $("#sp-flg").css("display")
-            ? M.css("left", "0%")
-            : M.css("left", 100 * e + "%"),
-            e > 0 && .02 >= e
-            ? ($("#control-bubble").removeClass(y),
-            m = _.attr("data-facility-type", "0"))
-            : e > .02 && .13 >= e
-            ? ($("#control-bubble").addClass(y),
-            m = _.attr("data-facility-type", "1"),
-            h = M.find("dd").text("Table Cofun"),
-            v = M.find("dt").text("テーブルコフン"),
-            p = M.find("a").attr("href", "#facility-1"))
-            : e > .13 && .3 >= e
-            ? ($("#control-bubble").addClass(y),
-            m = _.attr("data-facility-type", "3"),
-            h = M.find("dd").text("Crater Cofun"),
-            v = M.find("dt").text("すりばちコフン"),
-            p = M.find("a").attr("href", "#facility-3"))
-            : e > .3 && .5 >= e
-            ? ($("#control-bubble").addClass(y),
-            m = _.attr("data-facility-type", "5"),
-            h = M.find("dd").text("Fuwafuwa Cofun"),
-            v = M.find("dt").text("ふわふわコフン"),
-            p = M.find("a").attr("href", "#facility-5"))
-            : e > .5 && .75 >= e
-            ? ($("#control-bubble").addClass(y),
-            m = _.attr("data-facility-type", "2"),
-            h = M.find("dd").text("Stage Cofun"),
-            v = M.find("dt").text("ステージコフン"),
-            p = M.find("a").attr("href", "#facility-2"))
-            : e > .75 && .9 >= e
-            ? ($("#control-bubble").addClass(y),
-            m = _.attr("data-facility-type", "4"),
-            h = M.find("dd").text("Info&Lounge Cofun"),
-            v = M.find("dt").text("インフォ ＆ ラウンジコフン"),
-            p = M.find("a").attr("href", "#facility-4"))
-            : (m = _.attr("data-facility-type", "0"),
-            $("#control-bubble").removeClass(y))
-        }
+        }($(".wide-wrap"))]);
 
         $('#wide_ctrl_slider').slider({
             range: !1,
@@ -145,50 +94,43 @@ $(document).ready(function() {
             slide: function(e, t) {
                 b.pause(),
                 b.progress(t.value),
-                "block" == $("#sp-flg").css("display")
-                ? M.css("left", "0%")
-                : M.css("left", 100 * t.value + "%"),
-                t.value > 0 && t.value <= .02
-                ? ($("#control-bubble").removeClass(y),
-                m = _.attr("data-facility-type", "0"))
-                : t.value > .02 && t.value <= .13
-                ? ($("#control-bubble").addClass(y),
-                m = _.attr("data-facility-type", "1"),
-                h = M.find("dd").text("Table Cofun"),
-                v = M.find("dt").text("テーブルコフン"),
-                p = M.find("a").attr("href", "#facility-1"))
-                : t.value > .13 && t.value <= .3
-                ? ($("#control-bubble").addClass(y),
-                m = _.attr("data-facility-type", "3"),
-                h = M.find("dd").text("Crater Cofun"),
-                v = M.find("dt").text("すりばちコフン"),
-                p = M.find("a").attr("href", "#facility-3"))
-                : t.value > .3 && t.value <= .5
-                ? ($("#control-bubble").addClass(y),
-                m = _.attr("data-facility-type", "5"),
-                h = M.find("dd").text("Fuwafuwa Cofun"),
-                v = M.find("dt").text("ふわふわコフン"),
-                p = M.find("a").attr("href", "#facility-5"))
-                : t.value > .5 && t.value <= .75
-                ? ($("#control-bubble").addClass(y),
-                m = _.attr("data-facility-type", "2"),
-                h = M.find("dd").text("Stage Cofun"),
-                v = M.find("dt").text("ステージコフン"),
-                p = M.find("a").attr("href", "#facility-2"))
-                : t.value > .75 && t.value <= .9
-                ? ($("#control-bubble").addClass(y),
-                m = _.attr("data-facility-type", "4"),
-                h = M.find("dd").text("Info&Lounge Cofun"),
-                v = M.find("dt").text("インフォ ＆ ラウンジコフン"),
-                p = M.find("a").attr("href", "#facility-4"))
-                : ($("#control-bubble").removeClass(y),
-                m = _.attr("data-facility-type", "0"));
                 $("html, #wide_slider, .ui-slider-handle").one("mouseup", function(e) {
                     b.resume()
                 });
             }
         });
-        
-        //"block" == $("#sp-flg").css("display") ? M.css("left", "0%") : M.css("left", 100 * e + "%"), e > 0 && .02 >= e ? ($("#control-bubble").removeClass(y), m = _.attr("data-facility-type", "0")) : e > .02 && .13 >= e ? ($("#control-bubble").addClass(y), m = _.attr("data-facility-type", "1"), h = M.find("dd").text("Table Cofun"), v = M.find("dt").text("テーブルコフン"), p = M.find("a").attr("href", "#facility-1")) : e > .13 && .3 >= e ? ($("#control-bubble").addClass(y), m = _.attr("data-facility-type", "3"), h = M.find("dd").text("Crater Cofun"), v = M.find("dt").text("すりばちコフン"), p = M.find("a").attr("href", "#facility-3")) : e > .3 && .5 >= e ? ($("#control-bubble").addClass(y), m = _.attr("data-facility-type", "5"), h = M.find("dd").text("Fuwafuwa Cofun"), v = M.find("dt").text("ふわふわコフン"), p = M.find("a").attr("href", "#facility-5")) : e > .5 && .75 >= e ? ($("#control-bubble").addClass(y), m = _.attr("data-facility-type", "2"), h = M.find("dd").text("Stage Cofun"), v = M.find("dt").text("ステージコフン"), p = M.find("a").attr("href", "#facility-2")) : e > .75 && .9 >= e ? ($("#control-bubble").addClass(y), m = _.attr("data-facility-type", "4"), h = M.find("dd").text("Info&Lounge Cofun"), v = M.find("dt").text("インフォ ＆ ラウンジコフン"), p = M.find("a").attr("href", "#facility-4")) : (m = _.attr("data-facility-type", "0"), $("#control-bubble").removeClass(y))
+    }());
+
+    (function() {
+        var i = null;
+        var header = $(".wide-header");
+        header.on({
+            'mousemove': function() {
+                clearTimeout(i);
+                header.addClass('wide-shown');
+                header.css('cursor', 'auto');
+                i = setTimeout(function () {
+                    header.removeClass('wide-shown');
+                    header.css('cursor', 'none');
+                }, 1000);
+            },
+            'mouseleave': function() {
+                clearTimeout(i);
+                header.removeClass('wide-shown');
+                header.css('cursor', 'none');
+            }
+        });
+    }());
+
+    (function() {
+        var animData = {
+            wrapper: document.getElementById('wide_layer_3'),
+            animType: 'svg',
+            loop: true,
+            prerender: true,
+            autoplay: true,
+            path: '{{ "/img/entities/data.json" | prepend: site.baseurl }}'
+        };
+        var anim = bodymovin.loadAnimation(animData);
     })();
 })
