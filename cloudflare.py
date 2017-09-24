6a42865900e2a4d32af43164b8525b3e28501077
sjcl <fruit@bebehp.com>
2017-09-24 21:21:32 +0900

    Development Mode

---
+++ cloudflare.py
@@ -0,0 +1,17 @@
+import sys
+import requests
+
+
+def main():
+    args = sys.argv
+    headers = {
+        'Content-Type': 'application/json',
+        'X-Auth-Email': args[2],
+        'X-Auth-Key': args[3]
+    }
+    url = 'https://api.cloudflare.com/client/v4/zones/' + args[1] + '/settings/development_mode'
+    print(requests.patch(url, headers=headers, json={"value": "on"}).json())
+
+
+if __name__ == '__main__':
+    main()
